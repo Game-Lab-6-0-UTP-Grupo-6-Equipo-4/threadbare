3cc6dbc83b3157857729d5a93b39bb67ad9ac4bf
Tobías Romero <arebuayon@hotmail.com>
2026-06-25 03:37:59 -0300

    Agregadas nuevas casas

--- scenes/quests/story_quests/investigadora_cibern/2_void_runner/components/investigadora_cibern_tutorial_void_runner.gd
+++ scenes/quests/story_quests/investigadora_cibern/2_void_runner/components/investigadora_cibern_tutorial_void_runner.gd
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
+# investigadora_cibern void runner tutorial root
 extends Node2D
 
 @onready var spirit_thread: CollectibleItem = %SpiritThread
